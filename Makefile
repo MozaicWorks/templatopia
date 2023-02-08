f1c5c942be2d939fe54384597c4e0efbf1685057
Alex Bolboaca <alexboly@gmail.com>
2023-02-08 18:35:53 +0200

    Design the command line arguments

---
+++ Makefile
@@ -0,0 +1,5 @@
+run:
+	pipenv run python templatopia/cli.py
+
+test:
+	pipenv run python templatopia/cli.py --template template.svg --from-csv "in/list.csv" --to-path out --name-template "{{firstName}}-{{lastName}}.svg" --template-path template/ --map first_name:firstName --map last_name:lastName --map user_email:emailAddress
---
+++ templatopia/cli.py
@@ -0,0 +1,35 @@
+import argparse
+from pathlib import Path
+import sys
+import csv
+
+def main():
+    parser = argparse.ArgumentParser(
+                    prog = 'templatopia',
+                    description = 'Creates files from a template file and a table. The templating language use is Mustache.',
+    )
+    parser.add_argument("--template", required=True, help="The name of the template file. Eg. template.svg, template.eml")
+    parser.add_argument("--from-csv", required=False, help="The csv file containing the source list")
+    parser.add_argument("--to-path", required=False, default="out", help="The path where we write the output files. Defaults to `out`")
+    parser.add_argument("--name-template", required=False, default="{{index}}", help="The template for the output file. It uses the same mappings plus the `index` variable. Defaults to {{index}}")
+    parser.add_argument("--out-ext", required=False, help="The extension of the output file. Defaults to the extension of the template")
+    parser.add_argument("--map", required=False, action='append', help="Map from the input column name to the output variable. Eg. first_name:firstName")
+    parser.add_argument("--template-path", required=False, default=".", help="The path to the template")
+    
+    args = parser.parse_args()
+
+    templateFilePath = Path(args.template_path, args.template)
+    if(not templateFilePath.exists()):
+        sys.exit("Template file not found: {templateFilePath}".format(templateFilePath=templateFilePath))
+
+    csvFilePath = Path(args.from_csv)
+    if(not csvFilePath.exists()):
+        sys.exit("List file not found: {csvFilePath}".format(csvFilePath=csvFilePath))
+
+    outPath = Path(args.to_path)
+    if(not outPath.exists()):
+        outPath.mkdir()
+    
+
+if __name__ == "__main__":
+    main()
