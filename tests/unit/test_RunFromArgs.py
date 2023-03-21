a6b05982beb84a26ff8bcb8db1f444f94b9a82ec
Alex Bolboaca <alexboly@gmail.com>
2023-03-21 17:22:20 +0200

    python end to end test

--- tests/unit/test_RunFromArgs.py
+++ tests/unit/test_RunFromArgs.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass
+from pathlib import Path
 
 import pytest
 
@@ -8,13 +9,29 @@ from templatopia.RunFromArgs import RunFromArgs
 @dataclass
 class Args:
     map:str
-    map_from:str
+    from_csv:str
+    name_template:str
+    template:str
+    common_value:str
+    verbose:bool
+    common_values_from:str="common-values.json"
+    map_from:str="map.json"
+    template_path:str="./template/"
+    to_path:str="out/"
 
 class TestRunFromArgs:
-    def test_run(self):
-        with pytest.raises(FileNotFoundError):
-            args = Args(
-                    map = "",
-                    map_from = ""
-                    )
-            RunFromArgs(args).run()
+    @pytest.fixture
+    def test_run(self, tmp_path):
+        args = Args(
+                map = None,
+                from_csv = "in/list.csv",
+                name_template="{{first_name}}{{last_name}}",
+                template = "template.svg",
+                common_value = None,
+                to_path = str(tmp_path),
+                verbose = False
+                )
+        RunFromArgs(args).run()
+
+        assert Path(args.to_path, "Jane-Doe.svg").exists()
+        assert Path(args.to_path, "John-Doe.svg").exists()
